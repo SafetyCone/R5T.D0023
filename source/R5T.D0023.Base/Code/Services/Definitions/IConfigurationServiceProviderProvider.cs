a370d3d40f84f2911d05c04bfac3d09ffc5d7c99
MinexAutomation <minex.automation@gmail.com>
2020-07-01 22:54:08 -0700

    Initial commit of source-stub. Added R5T.D0023.Base library. Added IConfigurationServiceProviderProvider service definition interface type.

---
+++ source/R5T.D0023.Base/Code/Services/Definitions/IConfigurationServiceProviderProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.D0023.Base
+{
+    /// <summary>
+    /// Provides the <see cref="IServiceProvider"/> used during startup configuration.
+    /// </summary>
+    public interface IConfigurationServiceProviderProvider
+    {
+        IServiceProvider GetConfigurationServiceProvider();
+    }
+}
